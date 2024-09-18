7a20f538293bf6e4c24113a945a2a8cdb7cc3927
Rahul7740 <rahultaak7740@gmail.com>
2024-09-18 12:55:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Footer from "./common/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Home/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "../common/Header"
+function Banner() {
+  return (
+    <section>
+        <Header />
+        <div className="container">
+            banner
+        </div>
+    </section>
+  )
+}
+
+export default Banner
